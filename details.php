eff0f8d7052bed8e181b98bb1fff5bc08a211795
Mor-Gadi <87760812+Mor-Gadi@users.noreply.github.com>
2021-07-26 10:22:49 +0300

    Update details.php

--- details.php
+++ details.php
@@ -1,6 +1,23 @@
 <?php
-    $email = $_POST['email'];
-    syslog(LOG_INFO, "$email");
+$email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $email = test_input($_POST["email"]);
+    ttq.track('SubmitForm')
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>
+
+
+
 <html>
-<head>
+<body>
+Thank you for signing, additional details will be sent to: <?php echo $_POST["email"]; ?><br>
+</body>
+</html>
